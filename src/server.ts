3a76fd259de08ba8a23a74accd3010c6e1b365f9
José Henrique <zehenrique0822@gmail.com>
2023-02-25 10:35:40 -0300

    chore: add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import express from "express";
-import routes from "../routes";
+import express from 'express'
+import routes from '../routes'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(routes);
+app.use(express.json())
 
-app.listen(3333, () => console.log("🔥 Server started at http://localhost:3333"));
+app.use(routes)
+
+app.listen(3333, () => { console.log('🔥 Server started at http://localhost:3333') })
