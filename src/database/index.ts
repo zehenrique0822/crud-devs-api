efba411bcdc785a8a6abbc139415cc1314c6222f
José Henrique <zehenrique0822@gmail.com>
2023-02-26 15:14:19 -0300

    refactor: using .env variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,15 @@
 import { DataSource } from 'typeorm'
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'db',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
-  database: 'devsdb',
-  password: '1234',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: false,
   entities: [
     './src/models/*.ts'
--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,4 @@ app.use(express.json())
 
 app.use(routes)
 
-app.listen(3333, () => { console.log('🔥 Server started at http://localhost:3333') })
+app.listen(process.env.API_PORT ?? 3333, () => { console.log('🔥 Server started at http://localhost:3333') })
