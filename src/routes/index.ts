99cc75788adaf22930611f07d87e3d8acb3fb727
José Henrique <zehenrique0822@gmail.com>
2023-02-27 21:17:13 -0300

    fix: order of routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,10 @@ import levelsRouter from './levels.routes'
 import developersRouter from './developers.routes'
 const routes = Router()
 
+routes.use('/levels', levelsRouter)
+routes.use('/developers', developersRouter)
 routes.use('/', (req, res) => {
   res.send({ api: 'api-crud-developers' })
 })
-routes.use('/levels', levelsRouter)
-routes.use('/developers', developersRouter)
 
 export default routes
