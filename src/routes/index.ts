fc453de9ccf04f7084361862853fa72419da31d4
José Henrique <zehenrique0822@gmail.com>
2023-02-27 21:08:07 -0300

    feat: add route of test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import levelsRouter from './levels.routes'
 import developersRouter from './developers.routes'
 const routes = Router()
 
+routes.use('/', (req, res) => {
+  res.send({ api: 'api-crud-developers' })
+})
 routes.use('/levels', levelsRouter)
 routes.use('/developers', developersRouter)
 
