e9e97572e0660d2b76cf510e9e9ce7593e5fc1a0
José Henrique <zehenrique0822@gmail.com>
2023-03-02 23:53:59 -0300

    refactor: return status code 404 if not found

--- src/modules/developers/repositories/useCases/list/ListDevelopersController.ts
+++ src/modules/developers/repositories/useCases/list/ListDevelopersController.ts
@@ -16,10 +16,12 @@ class ListDevelopersController {
         limit: limit ?? 0
       })
 
+      if (!list.count) return response.status(404).json({ error: "Nenhum desenvolvedor encontrato!" })
+
       return response.status(200).json(list)
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(404).json({ error: err.message })
+        return response.status(400).json({ error: err.message })
       }
       return response.status(500).json({ error: err })
     }
--- src/modules/levels/repositories/useCases/list/ListLevelsController.ts
+++ src/modules/levels/repositories/useCases/list/ListLevelsController.ts
@@ -16,10 +16,12 @@ class ListLevelsController {
         limit: limit ?? 0
       })
 
+      if (!list.count) return response.status(404).json({ error: "Nenhum nível encontrato!" })
+
       return response.status(200).json(list)
     } catch (err) {
       if (err instanceof Error) {
-        return response.status(404).json({ error: err.message })
+        return response.status(400).json({ error: err.message })
       }
       return response.status(500).json({ error: err })
     }
